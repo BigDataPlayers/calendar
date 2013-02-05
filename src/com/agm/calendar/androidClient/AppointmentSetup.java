6e921b999196b52603f0f443bfd3fe81ecfa8b32
BigDataPlayers <bigdataplayers@gmail.com>
2013-02-04 21:58:53 -0500

    Visit options added
    Signed-off-by: BigDataPlayers <bigdataplayers@gmail.com>

--- src/com/agm/calendar/androidClient/AppointmentSetup.java
+++ src/com/agm/calendar/androidClient/AppointmentSetup.java
@@ -1,6 +1,7 @@
 package com.agm.calendar.androidClient;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,20 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIUtils;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Created with IntelliJ IDEA.
@@ -39,11 +34,11 @@ public class AppointmentSetup extends Activity implements View.OnClickListener {
     private static final String GET_OFFICE = "OOfficeList";
     private static final String GET_OPTIONS = "OOptionsList";
     private String officeNames, providerNames, optionsNames;
-    private ArrayList<String> officeList, providerList, optionsList ;
+    private ArrayList<String> officeList, providerList, optionsList;
 
     private CalendarListAdapter listAdapter;
     private ExpandableListView expandableList;
-
+    private ListView listV;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,14 +51,24 @@ public class AppointmentSetup extends Activity implements View.OnClickListener {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
         //get provider details from the server
         loadData();
+
+        //prepare listview for options
+        listV = (ListView) findViewById(R.id.optionList);
+        listV.setAdapter(new ArrayAdapter<String>(this, R.layout.visitoptions, optionsList));
+        listV.setTextFilterEnabled(true);
+        listV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(),((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //get reference to the ExpandableListView
         expandableList = (ExpandableListView) findViewById(R.id.providerList);
         //create the adapter by passing your ArrayList data
-        listAdapter = new CalendarListAdapter(this, officeList);
+        listAdapter = new CalendarListAdapter(this, providerList);
         //attach the adapter to the list
         expandableList.setAdapter(listAdapter);
 
@@ -78,46 +83,44 @@ public class AppointmentSetup extends Activity implements View.OnClickListener {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        officeNames = getDataFromServer (GET_OFFICE) ;
+        officeNames = getDataFromServer(GET_OFFICE);
         providerNames = getDataFromServer(GET_PROVIDER);
         optionsNames = getDataFromServer(GET_OPTIONS);
-        officeList = new ArrayList<String>(Arrays.asList(officeNames.split(" , ")));
-        providerList = new ArrayList<String>(Arrays.asList(providerNames.split(" , ")));
-        optionsList = new ArrayList<String>(Arrays.asList(optionsNames.split(" , ")));
-
-        System.out.println("Sizes officeNames:" + officeList + "\tproviderNames:" + providerList + "\toptionsNames:" + optionsList);
+        officeList = new ArrayList<String>(Arrays.asList(officeNames.split(",")));
+        providerList = new ArrayList<String>(Arrays.asList(providerNames.split(",")));
+        optionsList = new ArrayList<String>(Arrays.asList(optionsNames.split(",")));
 
     }
 
-    private String getDataFromServer (String command){
+    private String getDataFromServer(String command) {
         String inputLine = null;
         StringBuffer buffer = new StringBuffer();
         try {
             URL serverURL = new URL(SERVER_URL + command);
             URLConnection conn = serverURL.openConnection();
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            while ((inputLine = in.readLine()) != null)  buffer.append(inputLine);
+            while ((inputLine = in.readLine()) != null) buffer.append(inputLine);
             in.close();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         Log.i("Server call for " + SERVER_URL + command, buffer.toString());
-        return buffer.toString() ;
+        return buffer.toString();
     }
 
     private class CalendarListAdapter extends BaseExpandableListAdapter {
         private Context context;
-        private ArrayList<String> officeList;
+        private ArrayList<String> ddProviderList;
 
-        public CalendarListAdapter(Context context, ArrayList<String> officeList) {
+        public CalendarListAdapter(Context context, ArrayList<String> list) {
             this.context = context;
-            this.officeList = officeList;
+            this.ddProviderList = list;
         }
 
         @Override
         public Object getChild(int groupPosition, int childPosition) {
-            String office = officeList.get(groupPosition) ;
+            String office = ddProviderList.get(groupPosition);
             ArrayList<String> providers = getProviderList(office);
             return providers.get(childPosition);
         }
@@ -137,10 +140,9 @@ public class AppointmentSetup extends Activity implements View.OnClickListener {
             }
 
             TextView sequence = (TextView) view.findViewById(R.id.sequence);
-            System.out.println("childPosition: " + childPosition);
-            sequence.setText(childPosition);
+            sequence.setText((childPosition + 1) + ">\t");
             TextView childItem = (TextView) view.findViewById(R.id.childItem);
-            childItem.setText(providerList.get(childPosition));
+            childItem.setText(officeList.get(childPosition));
 
             return view;
         }
@@ -148,19 +150,19 @@ public class AppointmentSetup extends Activity implements View.OnClickListener {
         @Override
         public int getChildrenCount(int groupPosition) {
 
-//            ArrayList<DetailInfo> productList = officeList.get(groupPosition).getProductList();
-            return providerList.size();
+//            ArrayList<DetailInfo> productList = ddProviderList.get(groupPosition).getProductList();
+            return officeList.size();
 
         }
 
         @Override
         public Object getGroup(int groupPosition) {
-            return officeList.get(groupPosition);
+            return ddProviderList.get(groupPosition);
         }
 
         @Override
         public int getGroupCount() {
-            return officeList.size();
+            return ddProviderList.size();
         }
 
         @Override
@@ -178,7 +180,7 @@ public class AppointmentSetup extends Activity implements View.OnClickListener {
             }
 
             TextView heading = (TextView) view.findViewById(R.id.heading);
-            heading.setText(officeList.get(groupPosition));
+            heading.setText(ddProviderList.get(groupPosition));
 
             return view;
         }
@@ -195,36 +197,36 @@ public class AppointmentSetup extends Activity implements View.OnClickListener {
 
     }
 
-     private ArrayList<String> getProviderList (String office) {
-         //TODO - revisit once the data is pulled from real data store
-         return providerList;
-     }
+    private ArrayList<String> getProviderList(String office) {
+        //TODO - revisit once the data is pulled from real data store
+        return officeList;
+    }
 
 
     //our child listener
     private ExpandableListView.OnChildClickListener providerItemClicked =
             new ExpandableListView.OnChildClickListener() {
 
-        public boolean onChildClick(ExpandableListView parent, View v,
-                                    int groupPosition, int childPosition, long id) {
+                public boolean onChildClick(ExpandableListView parent, View v,
+                                            int groupPosition, int childPosition, long id) {
 
-            //get the group header
-            String headerInfo = officeList.get(groupPosition);
-            //get the child info
-            String detailInfo =  providerList.get(childPosition);
-            //display it or do something with it
-            Toast.makeText(getBaseContext(), "Clicked on Detail " + headerInfo + "/" + detailInfo, Toast.LENGTH_LONG).show();
-            return false;
-        }
-    };
+                    //get the group header
+                    String headerInfo = providerList.get(groupPosition);
+                    //get the child info
+                    String detailInfo = officeList.get(childPosition);
+                    //display it or do something with it
+                    Toast.makeText(getBaseContext(), "Clicked on Detail " + headerInfo + "/" + detailInfo, Toast.LENGTH_LONG).show();
+                    return false;
+                }
+            };
 
     //our group listener
-    private ExpandableListView.OnGroupClickListener officeItemClicked =  new ExpandableListView.OnGroupClickListener() {
+    private ExpandableListView.OnGroupClickListener officeItemClicked = new ExpandableListView.OnGroupClickListener() {
 
         public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
 
             //get the group header
-            String headerInfo = officeList.get(groupPosition);
+            String headerInfo = providerList.get(groupPosition);
             //display it or do something with it
             Toast.makeText(getBaseContext(), "Child on Header " + headerInfo, Toast.LENGTH_LONG).show();
 
